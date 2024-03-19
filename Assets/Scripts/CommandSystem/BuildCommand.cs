cb70a4ff9d8538849752f5c44cb369a7a2b0fe08
Alsy13 <113466872+Alsy13@users.noreply.github.com>
2024-03-19 19:12:21 +0100

    Spawnutí budovy, prefab, resource management
    2 a 3 bod

--- Assets/Scripts/CommandSystem/BuildCommand.cs
+++ Assets/Scripts/CommandSystem/BuildCommand.cs
@@ -1,17 +1,31 @@
+using Unity.VisualScripting;
 using UnityEngine;
+
 public class BuildCommand : Command
 {
     private GameObject buildingPrefab;
     private Tile tile;
+    private viewmanagement view;
 
-    public BuildCommand(GameObject buildingPrefab, Tile tile)
+    public BuildCommand(GameObject buildingPrefab, Tile tile, viewmanagement view)
     {
         this.buildingPrefab = buildingPrefab;
         this.tile = tile;
+        this.view = view;
     }
 
     public override void Execute()
     {
+        if (view != null)
+        {
+            view.RemoveMoney(100);
+            view.RemoveWood(100);
+        }
+        else
+        {
+            Debug.LogWarning("View is not set.");
+        }
+
         var building = GameObject.Instantiate(buildingPrefab);
         tile.SetBuilding(building);
     }
