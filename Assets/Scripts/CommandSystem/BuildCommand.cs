cceb8df52e7ea628da44fd2fe461388fcc46304d
error-v0e <Pej2202@seznam.cz>
2024-03-28 15:42:48 +0100

    2 budovy moznost střídaní
    -moznost se zadlužit když si spatně pročítáš finance

--- Assets/Scripts/CommandSystem/BuildCommand.cs
+++ Assets/Scripts/CommandSystem/BuildCommand.cs
@@ -4,29 +4,51 @@ using UnityEngine;
 public class BuildCommand : Command
 {
     private GameObject buildingPrefab;
+    private GameObject buildingPrefab2;
     private Tile tile;
     private viewmanagement view;
+    private int lastPressedButton;
 
-    public BuildCommand(GameObject buildingPrefab, Tile tile, viewmanagement view)
+    public BuildCommand(GameObject buildingPrefab, GameObject buildingPrefab2, Tile tile, viewmanagement view, int lastPressedButton)
     {
         this.buildingPrefab = buildingPrefab;
+        this.buildingPrefab2 = buildingPrefab2;
         this.tile = tile;
         this.view = view;
+        this.lastPressedButton = lastPressedButton;
     }
 
     public override void Execute()
     {
-        if (view != null)
+        if (lastPressedButton == 1)
         {
-            view.RemoveMoney(100);
-            view.RemoveWood(100);
-        }
-        else
-        {
-            Debug.LogWarning("View is not set.");
-        }
+            if (view != null)
+            {
+                view.RemoveMoney(100);
+                view.RemoveWood(100);
+            }
+            else
+            {
+                Debug.LogWarning("View is not set.");
+            }
 
-        var building = GameObject.Instantiate(buildingPrefab);
-        tile.SetBuilding(building);
+            var building = GameObject.Instantiate(buildingPrefab);
+            tile.SetBuilding(building);
+        }
+        else if (lastPressedButton == 2)
+        {
+            if (view != null)
+            {
+                view.RemoveMoney(200);
+                view.RemoveWood(200);
+            }
+            else
+            {
+                Debug.LogWarning("View is not set.");
+            }
+
+            var building = GameObject.Instantiate(buildingPrefab2);
+            tile.SetBuilding(building);
+        }
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,15 +1,33 @@
-using UnityEngine;
+ï»¿using UnityEngine;
+using UnityEngine.UI;
 
 public class Tile : MonoBehaviour
 {
     viewmanagement view;
     public TileData TileData { get; set; }
-    public GameObject buildingPrefab; // Promìnná pro uložení prefabu
+    public GameObject buildingPrefab;
+    public GameObject buildingPrefab2;
+    private static int lastPressedButton = 1;
+
+    public void OnButton1Pressed()
+    {
+        lastPressedButton = 1;
+        Debug.Log("Button 1 pressed");
+        Debug.Log("pressed " + lastPressedButton);
+    }
+
+    public void OnButton2Pressed()
+    {
+        lastPressedButton = 2;
+        Debug.Log("Button 2 pressed");
+        Debug.Log("pressed " + lastPressedButton);
+    }
+
 
     internal void SetBuilding(GameObject building)
     {
         building.transform.SetParent(transform);
-        building.transform.localPosition = new Vector3(1.28f, 0.8f, 1.19f);
+        building.transform.localPosition = new Vector3(1.25f, 0.5f, 1.25f);
     }
 
     void Awake()
@@ -22,25 +40,28 @@ public class Tile : MonoBehaviour
         if (view != null && (view.wood > 0 || view.money > 0))
         {
             // Kontrola, zda je nastaven prefab
-            if (buildingPrefab != null)
+            if (buildingPrefab != null && buildingPrefab2 != null)
             {
-                // Spawnout prefab místo kostky
+                Debug.Log("stavise " + lastPressedButton);
+                // Spawnout prefab mÃ­sto kostky
                 var building = GameObject.Instantiate(buildingPrefab);
+                var building2 = GameObject.Instantiate(buildingPrefab2);
                 var cmd = new BuildCommand(
                     building,
+                    building2,
                     this,
-                    view
-                );
+                    view,
+                    lastPressedButton);
                 CommandQueue.Instance.AddCommand(cmd);
             }
             else
             {
-                Debug.LogWarning("Prefab pro stavbu není nastaven.");
+                Debug.LogWarning("Prefab pro stavbu nenÃ­ nastaven.");
             }
         }
         else
         {
-            Debug.LogWarning("Není dostatek døeva nebo penìz pro spawn stavby.");
+            Debug.LogWarning("NenÃ­ dostatek dÃ¸eva nebo penÃ¬z pro spawn stavby.");
         }
     }
 }
